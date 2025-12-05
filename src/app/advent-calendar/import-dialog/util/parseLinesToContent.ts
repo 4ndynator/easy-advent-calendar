6b7d2b2af08248ae626648d20433950fec2e1fd5
Andreas Lautner <andy.lautner@gmail.com>
2025-12-05 17:44:30 +0100

    fix parser

--- src/app/advent-calendar/import-dialog/util/parseLinesToContent.ts
+++ src/app/advent-calendar/import-dialog/util/parseLinesToContent.ts
@@ -3,10 +3,11 @@ import {AdventCalendarDoor} from '../../model/advent-calendar-door';
 export function parseLinesToContent(input: string): AdventCalendarDoor[] {
   return input
     .split('\n')
+    .map(line => line.trim())
+    .filter(line => line.length > 0)
     .map((line, index) => ({
       day: index + 1,
       title: '',
-      description: line.trim()
-    }))
-    .filter(door => door.description.length > 0);
+      description: line
+    }));
 }
